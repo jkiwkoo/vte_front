8eb683f2e0846824783d17246480d699180127e6
95decode <95decode@gmail.com>
2023-08-27 04:38:05 +0900

    Add company history

--- src/lang/English.ts
+++ src/lang/English.ts
@@ -47,6 +47,65 @@ const English = {
     '7': 'Inspection/Cleaning',
     '8': 'Packing/Shipment',
   },
+  company: {
+    title: 'COMPANY HISTORY',
+    '2012': {
+      '1': '• Construct and produce reinforced glass in Suzhou.',
+      '2': '- Exclusive contractor of TYE.',
+      '3': '- Supply entire quantity of GS / GV / CS / SDC series.',
+      '4': '• Supplied Samsung SDC TABLET PC COVER GLASS (ELK).',
+    },
+    '2013': {
+      '1': '• Establish corporation of HDC in China.',
+      '2': '• Construct mass production line for JCOP Optical lens.',
+      '3': '• Establish VTE CORP, developed a glass for Samsung Electronics (Print division).',
+      '4': '• Develop subsidiary materials and supplied to Hitechno-cell Corp Print division.',
+      '5': '• Registered to official supplier of Samsung Electronics (print division).',
+    },
+    '2014': {
+      '1': '• Supplied silicon conductive tape (Samsung electronics print division).',
+    },
+    '2015': {
+      '1': '• Contract MOU with Suzhou Padnic Conductive Material Co., Ltd.',
+      '2': '- Supplied Graphite / silicon PAD (LGD).',
+    },
+    '2017': {
+      '1': '• Samsung Electronics (Consumer Electronics division)',
+      '2': 'parts registration approved (ELK in progress)',
+      '3': '• Registered to official supplier of LG Electronics(Consumer Electronics division)',
+      '4': '(Exclusive supplier of SMART INSTA VIEW model)',
+    },
+    '2018': {
+      '1': '• Registered to HP Print solution vender (Tactic supplier of LSU_Polygon Mirror)',
+      '2': '• Supply furniture glass (IKEA) / exported to Vietnam',
+      '3': '• Supply IVI glass , HUD cover glass (HYUNDAI MOBIS)',
+    },
+    '2019': {
+      '1': '• Tactic supplier of collimator glass HP / regiesterd aspherical rens for Canon.',
+      '2': '• Exclusive supplier of Samsung Electronics (Consumer Electronics division)',
+      '3': 'family hub 4.0 model.',
+      '4': '• Mass production supplier of LG Electronics CineBeam 4K model.',
+      '5': '• Participate develop project of lens(CONIC type) with HYUNDAI.',
+    },
+    '2020': {
+      '1': '• Supply quartz for manufacturing artificial optics. (TE Bios)',
+      '2': '• Supply robby phone supply (KD Navien).',
+      '3': '• Supply 3D GLASS etching/pattern GLASS (Astell&Kern)',
+    },
+    '2021': {
+      '1': '• Supply quartz glass supply (KD Network).',
+      '2': '• Supply quartz glass (KD Everon).',
+      '3': '• Supply quartz glass (KD Navien).',
+      '4': '• Supply OVEN GLASS (Samsung Electronics SEMA CORP).',
+    },
+    '2023': {
+      '1': '• Supply and developed 3D air hood for kitchen oven (KD Navien).',
+      '2': '• Develop Borosilicate Glass and UV Glass (Samsung Electronics).',
+      '3': '• Develop vehicle sensor glass (in progress)',
+      '4': '• Supply glass for FCBGA.',
+      '5': '• Develop Composite Material and Specialty Glass.',
+    },
+  },
 };
 
 export default English;

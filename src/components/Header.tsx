4861fce9760d74ac28e6ab430d5c94e2b524c7da
95decode <95decode@gmail.com>
2023-08-27 03:49:30 +0900

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,30 +25,61 @@ export const Header = ({ isBlack }: HeaderProps) => {
         }}
       />
       <div className="flex items-center">
-        <div className="flex h-[100px] pt-[50px] mr-[14vw] gap-[3.7vw] text-[18px] font-apple text-white">
-          <div
-            className="hover:cursor-pointer"
-            onClick={() => {
-              router.push('/company');
-            }}
-          >
-            {t('word.company')}
+        <div
+          className={`flex h-[100px] pt-[50px] mr-[14vw] gap-[3.7vw] text-[18px] font-apple ${
+            isBlack ? 'text-vte-gray-2' : 'text-white'
+          } `}
+        >
+          <div>
+            <div
+              className={`hover:cursor-pointer ${
+                isBlack && router.asPath === '/company' ? 'text-white' : null
+              }`}
+              onClick={() => {
+                router.push('/company');
+              }}
+            >
+              {t('word.company')}
+            </div>
+            <div
+              className={`${
+                isBlack && router.asPath === '/company' ? 'border' : null
+              }`}
+            ></div>
           </div>
-          <div
-            className="hover:cursor-pointer"
-            onClick={() => {
-              router.push('/products');
-            }}
-          >
-            {t('word.products')}
+          <div>
+            <div
+              className={`hover:cursor-pointer ${
+                isBlack && router.asPath === '/products' ? 'text-white' : null
+              }`}
+              onClick={() => {
+                router.push('/products');
+              }}
+            >
+              {t('word.products')}
+            </div>
+            <div
+              className={`${
+                isBlack && router.asPath === '/products' ? 'border' : null
+              }`}
+            ></div>
           </div>
-          <div
-            className="hover:cursor-pointer"
-            onClick={() => {
-              router.push('/support');
-            }}
-          >
-            {t('word.support')}
+          <div>
+            <div
+              className={`hover:cursor-pointer ${
+                isBlack && router.asPath === '/support' ? 'text-white' : null
+              }`}
+              onClick={() => {
+                router.push('/support');
+              }}
+            >
+              {t('word.support')}
+            </div>
+            <div
+              className={`${
+                isBlack && router.asPath === '/support' ? 'border' : null
+              }`}
+            ></div>
           </div>
         </div>
         <div className="h-[100px] pt-[40px] mr-[3.2vw]">

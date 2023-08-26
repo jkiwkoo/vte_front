b25e4bc301035762ec51a385bc8bb1dca3311e46
95decode <95decode@gmail.com>
2023-08-27 01:57:56 +0900

    Update languages

--- src/lang/Korean.ts
+++ src/lang/Korean.ts
@@ -7,6 +7,45 @@ const Korean = {
     product: 'PRODUCT',
     contact: 'CONTACT',
   },
+  glass: {
+    '1': {
+      title: 'Optical Lens',
+      subTitle: 'Aspherical Lens',
+      description: 'FOO',
+    },
+    '2': {
+      title: '2D/3D Glass',
+      subTitle: 'Home Appliance',
+      description: 'BAR',
+    },
+    '3': {
+      title: 'Touch Glass',
+      subTitle: 'In-vehicle Infotainment',
+      description: 'BAZ',
+    },
+    '4': {
+      title: 'Hybrid Glass',
+      subTitle: 'Low-Emissivity Glass',
+      description: 'QUX',
+    },
+  },
+  screen: {
+    '1': 'Aspherical Lens',
+    '2': 'Home Appliance',
+    '3': 'Hybrid Glass',
+  },
+  flow: {
+    title: 'We are a pioneer in Glass Technology.',
+    subTitle: 'VTE Corp is a leading innovator in materials industry',
+    '1': 'Raw materials of Glass',
+    '2': 'Glass Cutting',
+    '3': 'CNC',
+    '4': 'Temper',
+    '5': 'Press',
+    '6': 'AF Coating (Anti-Fingerprint)',
+    '7': 'Inspection/Cleaning',
+    '8': 'Packing/Shipment',
+  },
 };
 
 export default Korean;

901de27dfbd97fba47a8d16f519bbe160350ed72
95decode <95decode@gmail.com>
2023-08-29 23:46:28 +0900

    Modify z-index

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Contact = () => {
   return (
     <div
-      className="z-30 w-[120px] h-[120px] bg-white rounded-full flex items-center justify-center hover:cursor-pointer fixed bottom-[50px] right-[110px]"
+      className="z-50 w-[120px] h-[120px] bg-white rounded-full flex items-center justify-center hover:cursor-pointer fixed bottom-[50px] right-[110px]"
       onClick={() => {
         window.open('https://pf.kakao.com/_yxjsAG/chat');
       }}

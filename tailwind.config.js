b39d38251b112275c3f83494d798d252fbdd20bc
95decode <95decode@gmail.com>
2023-09-02 14:19:30 +0900

    add fade in animation 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,20 @@ module.exports = {
             transform: 'translateY(0)'
           },
         },
+        'fade-in-down2': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '50%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
         'fade-in-left': {
           '0%': {
             opacity: '0',
@@ -47,6 +61,7 @@ module.exports = {
       },
       animation: {
         'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-in-down2': 'fade-in-down2 1s ease-out',
         'fade-in-left': 'fade-in-left 0.5s ease-out'
       },
     },

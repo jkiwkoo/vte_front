b47d643d16f6da3fd94d64c572e539b764fe2c2b
95decode <95decode@gmail.com>
2023-09-16 20:41:44 +0900

    Modify contact size

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Contact = () => {
   return (
     <div
-      className="z-50 w-[120px] h-[120px] bg-white rounded-full flex items-center justify-center hover:cursor-pointer fixed bottom-[50px] right-[110px]"
+      className="z-50 w-[80px] h-[80px] bg-white rounded-full flex items-center justify-center hover:cursor-pointer fixed bottom-[100px] right-[150px]"
       onClick={() => {
         window.open('https://pf.kakao.com/_yxjsAG/chat');
       }}
@@ -11,8 +11,8 @@ export const Contact = () => {
       <Image
         src={`/images/Contact.png`}
         alt="contact"
-        width={73}
-        height={72}
+        width={49}
+        height={48}
         priority
       />
     </div>

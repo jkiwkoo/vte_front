ca4c5ba23b312eb5ef202d954c2e3ede8f37f6af
95decode <95decode@gmail.com>
2023-09-17 21:38:44 +0900

    Add support detail component

--- src/pages/support/index.tsx
+++ src/pages/support/index.tsx
@@ -1,6 +1,7 @@
 import { ContactUs } from '@/components/ContactUs';
 import { CustomerSupport } from '@/components/CustomerSupport';
 import { Header } from '@/components/Header';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -26,6 +27,116 @@ const Support = () => {
           <div>{t('support.description.1')}</div>
           <div>{t('support.description.2')}</div>
           <div>{t('support.description.3')}</div>
+        </div>
+        <div className="flex justify-center mb-[5.625vw]">
+          <div className="w-[15.625vw] h-[17.708vw] relative">
+            <div className="absolute z-30 flex flex-col items-start justify-end w-full h-full p-[1.5vw] text-white font-notoSans">
+              <div className="text-[24px] font-bold mb-[5px]">
+                {t('glass.1.title')}
+              </div>
+              <div
+                className="text-[16px] hover:cursor-pointer"
+                onClick={() => {
+                  router.push({
+                    pathname: `/products`,
+                    query: {
+                      product: 1,
+                    },
+                  });
+                }}
+              >
+                {t('word.more')}
+              </div>
+            </div>
+            <Image
+              src={'/images/SupportProduct1.png'}
+              fill
+              priority
+              quality={100}
+              alt="SupportProduct"
+            />
+          </div>
+          <div className="w-[15.625vw] h-[17.708vw] relative">
+            <div className="absolute z-30 flex flex-col items-start justify-end w-full h-full p-[1.5vw] text-white font-notoSans">
+              <div className="text-[24px] font-bold mb-[5px]">
+                {t('glass.2.title')}
+              </div>
+              <div
+                className="text-[16px] hover:cursor-pointer"
+                onClick={() => {
+                  router.push({
+                    pathname: `/products`,
+                    query: {
+                      product: 2,
+                    },
+                  });
+                }}
+              >
+                {t('word.more')}
+              </div>
+            </div>
+            <Image
+              src={'/images/SupportProduct2.png'}
+              fill
+              priority
+              quality={100}
+              alt="SupportProduct"
+            />
+          </div>
+          <div className="w-[15.625vw] h-[17.708vw] relative">
+            <div className="absolute z-30 flex flex-col items-start justify-end w-full h-full p-[1.5vw] text-white font-notoSans">
+              <div className="text-[24px] font-bold mb-[5px]">
+                {t('glass.3.title')}
+              </div>
+              <div
+                className="text-[16px] hover:cursor-pointer"
+                onClick={() => {
+                  router.push({
+                    pathname: `/products`,
+                    query: {
+                      product: 3,
+                    },
+                  });
+                }}
+              >
+                {t('word.more')}
+              </div>
+            </div>
+            <Image
+              src={'/images/SupportProduct3.png'}
+              fill
+              priority
+              quality={100}
+              alt="SupportProduct"
+            />
+          </div>
+          <div className="w-[15.625vw] h-[17.708vw] relative">
+            <div className="absolute z-30 flex flex-col items-start justify-end w-full h-full p-[1.5vw] text-white font-notoSans">
+              <div className="text-[24px] font-bold mb-[5px]">
+                {t('glass.4.title')}
+              </div>
+              <div
+                className="text-[16px] hover:cursor-pointer"
+                onClick={() => {
+                  router.push({
+                    pathname: `/products`,
+                    query: {
+                      product: 4,
+                    },
+                  });
+                }}
+              >
+                {t('word.more')}
+              </div>
+            </div>
+            <Image
+              src={'/images/SupportProduct4.png'}
+              fill
+              priority
+              quality={100}
+              alt="SupportProduct"
+            />
+          </div>
         </div>
         <div className="flex justify-center gap-[3.5vw] text-white text-[24px] mb-[3.385vw]">
           <div
